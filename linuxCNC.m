3f54c55a5332c90364cba491e38855f0bb8bff29
Matt <87336155+Matttab@users.noreply.github.com>
2021-12-30 22:19:37 +0100

    Add files via upload

---
+++ linuxCNC.m
@@ -0,0 +1,148 @@
+classdef linuxCNC < handle
+% Some routines taken from linuxCNC source code to interpolate splines
+% Code converted from C++ to ocatve (may code translation errors now)
+% 
+% Math reference:
+% https:%en.wikipedia.org/wiki/B%C3%A9zier_curve
+
+
+properties
+  metric=true;
+   _pos_x;
+   _pos_y;
+   _pos_z;
+   _pos_a;
+   _pos_b;
+   _pos_c;
+   _pos_u;
+   _pos_v;
+   _pos_w;
+end %properties
+
+methods 
+function [x,y]=SPLINE_FEED2( obj, x1,  y1,  x2,  y2) 
+     x0 = obj._pos_x;
+     y0 = obj._pos_y;
+    % Tanget slope is (y1-y0)/(x1-x0) at start
+    % Tanget slope is (y2-y1)/(x2-x1) at end
+    fprintf(stderr, "SPLINE_FEED(quadratic): %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f\n",
+          x0,y0,x1,y1,x2,y2);
+
+    for i=0:100 % do 100 straight line per segment  
+       t = i / 100.;
+       % quadratic Bezier base
+       t2 = t*t;
+       t1 = 2*t*(1-t);
+       t0 = (1-t)*(1-t);
+       x(i+1,1) = x0*t0 + x1*t1 + x2*t2;
+       y(i+1,1) = y0*t0 + y1*t1 + y2*t2;
+      % EBo -- replace 12345 with *whatever* gives us the line_number
+      % STRAIGHT_FEED(12345, x,y, _pos_z, _pos_a, _pos_b, _pos_c, _pos_u, _pos_v, _pos_w);
+    end
+end
+
+function [x,y]=SPLINE_FEED3( obj, x1,  y1,  x2,  y2,  x3,  y3) 
+     x0 = obj._pos_x;
+     y0 = obj._pos_y;
+    % Tanget slope is (y1-y0)/(x1-x0) at start
+    % Tanget slope is (y3-y2)/(x3-x2) at end
+    fprintf(stderr, "SPLINE_FEED(cubic): %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f\n",
+          x0,y0,x1,y1,x2,y2,x3,y3);
+          
+    for i=0:100 % do 100 straight line per segment  
+      % cubic Bezier base for order n=3
+      % bi,n = (n k)*t^i*(1-t)^(n-i)
+      % start point interpolation property: start at t1(0)=1
+      % end   point interpolation property: end   at t3(0)=1
+       t = i / 100;
+       t3 = t*t*t;            % t3(0)=0[3x] and                 t3(1)=1[3] start point
+       t2 = 3*t*t*(1-t);      % t3(0)=0[2x] and t3(1)=0[1x]                             t2(1/2)=3/8
+       t1 = 3*t*(1-t)*(1-t);  % t3(0)=0[1x] and t3(1)=0[2x]                             t2(1/2)=3/8
+       t0 = (1-t)*(1-t)*(1-t);%             and t3(1)=0[3x] and t3(0)=1[3] endpoint     t2(1/2)=3/8
+       x(i+1,1) = x0*t0 + x1*t1 + x2*t2 + x3*t3;
+       y(i+1,1) = y0*t0 + y1*t1 + y2*t2 + y3*t3;
+      % EBo -- replace 12345 with *whatever* gives us the line_number
+      % STRAIGHT_FEED(12345, x,y, _pos_z, _pos_a, _pos_b, _pos_c, _pos_u, _pos_v, _pos_w);
+    end
+end
+% 
+% C=[ 1 2 3 4; 1 1.4 1.5 1.55]'
+% pts= construct_spline(C)
+function [x, y]=construct_spline(obj,input)
+   C=input';
+    %4 coeffs. for cubic spline a=t0,b=t1,c=t2,d=t3
+    n = size(C,1)-1; % number of splines
+    S=zeros(n,2,4);   % 2dim = x,y, 
+    for dim=1:2  %x,y loop
+        % define d:
+          d(1)=3.0*(C(2,dim)-C(1,dim));
+        for i=2:n
+          d(i)=3.0*(C(i+1,dim)-C(i-1,dim));
+        end
+          d(n+1)=3.0*(C(n+1,dim)-C(n,dim));
+          %  forward sweep: simplified tridiagonal solution:
+          % ball a(i)=1 and all c(i)=1
+        
+            b(1)=2.0;
+        for i=2:n+1
+            w = 1.0/b(i-1);
+            b(i) = 4.0 - w;
+            d(i) -= (w*d(i-1));
+        end
+        % calculate solution vector x(i) = D(i):
+        % (Wikipedia x() = Wolfram D())
+        x(n+1)=d(n+1)/b(n+1);
+        for i=n:-1:1
+            x(i)=(d(i)-x(i+1))/b(i);
+         end
+        % calculate spline S(i)(dim) a, b, c and d:
+        for i=1:n
+            S(i,dim,1)=C(i,dim);
+            S(i,dim,2)=x(i);
+            S(i,dim,3)=3.0*(C(i+1,dim) -C(i,dim)) - 2.0*x(i) - x(i+1);
+            S(i,dim,4)=2.0*(C(i,dim)   -C(i+1,dim)) + x(i) + x(i+1);
+        end
+    end
+        for p=1:n  %spline points
+    
+          for k=0:11  %time 0-1 for each spline
+            t=k*0.1; 
+             
+             % x,y = d*t^3 + c*t^2 + b*t + a;
+            x(p,k+1) = ((S(p,1,4)*t +S(p,1,3))*t +S(p,1,2))*t +S(p,1,1);
+            y(p,k+1) = ((S(p,2,4)*t +S(p,2,3))*t +S(p,2,2))*t +S(p,2,1);
+            %std::cout<< "x:" << px << " y:" << py << std::endl;          
+          end
+        end
+
+end
+    
+
+% template for NURBS_FEED
+% Code not functional
+function NURBS_FEED(obj, nurbs_control_points, k) 
+     u = 0.0;
+     n = length(nurbs_control_points) - 1;
+     umax = n - k + 2;
+     div = (n+1)*15;
+     knot_vector = zeros(n,k);	
+     P1 =[];
+    while (u+umax/div < umax) 
+        P1 = nurbs_point(u+umax/div, k, nurbs_control_points, knot_vector);
+        % EBo -- replace 12345 with *whatever* gives us the line_number
+        %STRAIGHT_FEED(12345, P1.X,P1.Y, _pos_z, _pos_a, _pos_b, _pos_c, _pos_u, _pos_v, _pos_w);
+        u = u + umax/div;
+    end 
+    P1(1) = nurbs_control_points(1,n);
+    P1(2) = nurbs_control_points(2,n);
+    % EBo -- replace 12345 with *whatever* gives us the line_number
+    % STRAIGHT_FEED(12345, P1.X,P1.Y, _pos_z, _pos_a, _pos_b, _pos_c, _pos_u, _pos_v, _pos_w);
+    knot_vector=[];
+end
+
+end %end methods
+
+methods (Static)
+end %end static methods
+
+end %class
---
+++ linuxCNCsplineInterpretation.m
@@ -0,0 +1,113 @@
+% Test of some routines from linuxCNC source code to interpolate splines
+% Check of spline construction integrity
+% Spline from P0 to P3 with interior points P1,P2 as waypoint/node/knot
+%
+% SPLINE_FEED with 6 input arguments require as input
+% the control points of a B-Spline. P0 and P3 are as well
+% use as first and last control points, however, only
+% [P1,P2,P3] are passed to SPLINE_FEED as arguments.
+
+% P1 and P2 define the shape of the spline
+% There are four shape parameters P1x, P1y, P2x, P2y
+% Likely the x-grid is always uniform from 
+% P0123 = P0+[0 1/3 2/3 1]*(P3-P0)];
+
+% x0,y0 Start point, stored in class property _pos_x and _pos_y
+%
+% Spline defition in Bezier format. Arguments of SPLINE_FEED 
+% y1 first coeff of cubic B-Spline
+% y2 second coeff of cubic B-Spline
+% y3 third coeff of cubic B-Spline
+
+% x0,y0 first control point on outer polygon = Start point P0
+% x1,y1 second control point on outer polygon = P1
+% x2,y2 third control point on outer polygon  = P2
+% x3,y3 last control point on outer polygon = End point P3
+
+clear all; close all;
+mySpline = linuxCNC;
+
+mySpline.metric=true;
+x0=0;
+y0=0;
+mySpline._pos_x=x0;
+mySpline._pos_y=y0;
+
+% P123 =  [I, P, endpt_x; J, Q, endpt_y]
+P123  = [1.0, 2.0, 3.0,;
+         1.0, 1.7, 1.8]; 
+P0123=[[x0; y0] P123];
+
+% do a spline interpolation separately for x and y
+t=linspace(0,1,length(P0123));
+tt=linspace(0,1,length(P0123)*10+1);
+ppx=splinefit(t,P0123(1,:), t ,"order", 3); 
+ppy=splinefit(t,P0123(2,:), t ,"order", 3); 
+xxp=ppval(ppx,  tt);
+yyp=ppval(ppy,  tt);
+
+% use x as parameter/breaks and fit a spline to y data
+pp=splinefit(P0123(1,:), P0123(2,:),t ,"order", 3); 
+xx=tt*(P0123(1,end)-P0123(1,1))+P0123(1,1);
+yy=ppval(pp,xx);
+% from octave
+% pp =  [ 1.69  0.38 0.01 0.0; % first segment is almost linear
+%        -2.60  2.08 0.91 1.37;
+%         0.05 -0.52 1.44 0.58]
+
+% matlab comparison
+% pp= spline(P0123(1,:), P0123(2,:));
+% also with breaks = [0 1 2 3 4];
+% will give other coefficients than octave splinefit
+% pp =  [-0.05  0.00 1.05 0.0; % first segment is almost linear
+%        -0.05 -0.15 0.90 1.0;
+%        -0.05 -0.30 0.45 1.7]
+
+% Conversion to B-Spline using Matlab
+% bpp = fn2fm(pp, 'B-', 0.1)
+% will change the breaks to knots with 4-fold multiplicity at start and end pt
+% bpp.knots = [0 0 0 0 3 3 3 3]
+bpp.coefs = [0 1.05 2.1 1.8] ; %the corners of the control polygon y-coordinates
+[xb,yb]=mySpline.SPLINE_FEED3(1, bpp.coefs(2), 2, bpp.coefs(3), 3, bpp.coefs(4) ); 
+
+% For a set of G5 codes
+% Several splines in a row        
+[x4, y4]= mySpline.construct_spline(P0123);
+
+% We want only one spline segment between start and endpt
+[x, y]= mySpline.construct_spline([P0123(:,1) P0123(:,4)]);
+  
+         
+ctrlPt(:,1)=P0123(:,1);       
+ctrlPt(:,2)=[1; 2];   
+ctrlPt(:,3)=[1.05; 2.1];   
+ctrlPt(:,4)=P0123(:,4);
+         
+[xq,yq]=mySpline.SPLINE_FEED2(ctrlPt(1,2), ctrlPt(2,2),   ctrlPt(1,4),  ctrlPt(2,4)  );
+[xc,yc]=mySpline.SPLINE_FEED3(ctrlPt(1,2), ctrlPt(2,2),   ctrlPt(1,3),  ctrlPt(2,3),  ctrlPt(1,4),ctrlPt(2,4) ); 
+
+figure; hold on;
+plot(P0123(1,:), P0123(2,:),'-o');
+
+%plot(xq,yq,'--r');
+plot(x,y,':k');
+plot(xb,yb,'-c');
+%plot(xc,yc,'-g');
+plot(xx,yy,'-m');
+%plot(xxp,yyp,'-r');
+plot(P0123(1,:),bpp.coefs ,'-dc');
+
+for k=1:3
+ plot(x4(k,:),y4(k,:),':k');
+end
+legend({'nodes',  ...
+'spline constructor (output of 1 splines from start to end pt)', ...
+'SPLINE FEED3(x of nodes, coefs from MATLAB converson to B-Spline)', ...
+'cubic octave fit explicit y=y(x)', ...
+'Coefs=control polygon from matlab conversion to B-Spline', 'spline constructor (output of 3 splines)',}, 'location','southeast'  );
+
+
+
+
+
+
